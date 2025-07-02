4e8148e5d03038a281fba74132538c3dc01b35f1
baikho228 <temuribaikhoidze11@gmail.com>
2025-07-02 05:12:26 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+   
+    
+    const carsArea = document.getElementById('cars-area');
+
+    fetch('https://stepprojects.ge/api/Car')
+      .then(res => res.json())
+         .then(cars => renderCars(cars))
+       {
+      };
+
+  
+    function renderCars(cars) {
+      carsArea.innerHTML = '';
+      cars.forEach(car => {
+      
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.dataset.id = car.id;
+
+        card.innerHTML = `
+          <img src="${car.imageUrl2 || 'https://via.placeholder.com/220x140'}" alt="">
+          <div class="card-body">
+            <h5>Модель: ${car.model}</h5>
+               <h5>Цена в день: $${car.dailyPrice}</h5>
+            <h5>Мест: ${car.seats}</h5>
+            <h5>Цвет: ${car.color}</h5>
+          </div>
+          <button onclick="removeCar(this)">delete</button>
+        `;
+ 
+        
+        carsArea.appendChild(card);
+
+      });
+    }
+
+
+    function removeCar(btn) {
+      const card = btn.parentElement;
+      const carId = card.dataset.id;
+      card.remove();
+      
+     
+
+      
+    }
